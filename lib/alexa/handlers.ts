1744dca3b6128654851acf4ad8fc5f8c848c0436
wintus <yuya0726+GitHub@gmail.com>
2018-08-08 03:34:48 +0900

    Add response about discomfort index

--- lib/alexa/handlers.ts
+++ lib/alexa/handlers.ts
@@ -12,12 +12,37 @@ export const GetTempIntentHandler: RequestHandler = {
   },
   handle(handlerInput) {
     const { point, hour, min, temp, humid } = dummyData();
-    const text = `${point}の${hour}時${min}分現在の温度は${temp}度、湿度は${humid}%です。`;
+    const index = discomfortIndex(temp, humid);
+    const feel = feeling(index);
+    const text =
+      `${point}の${hour}時${min}分現在の温度は${temp}度、湿度は${humid}%です。` +
+      `不快指数は${index}です。${feel}`;
 
     return handlerInput.responseBuilder
       .speak(text)
       .withSimpleCard(skillName, text)
       .getResponse();
+  }
+};
+
+// index = discomfort index
+const feeling = (index: number) => {
+  if (85 <= index) {
+    return "暑くてたまらないですねー";
+  } else if (80 <= index && index < 85) {
+    return "暑くて汗が出ますねー";
+  } else if (75 <= index && index < 80) {
+    return "ちょっと暑いですねー";
+  } else if (70 <= index && index < 75) {
+    return "暑くはないですねー";
+  } else if (65 <= index && index < 70) {
+    return "過ごしやすいですねー";
+  } else if (60 <= index && index < 65) {
+    return "何も感じません。";
+  } else if (65 <= index && index < 70) {
+    return "少し肌寒いですね。";
+  } else if (index < 55) {
+    return "寒いです…";
   }
 };
 
